4c6228b2bcdffd898865825ad51d8ef60cf87a75
innovacion3 <innovaciontecnologica3@hvvaldecilla.es>
2025-09-30 14:51:27 +0200

    ahora estoy empezando con las funciones

---
+++ Basic/Innovacion/14-loops.js
@@ -0,0 +1,15 @@
+// BUCLES
+
+//for
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+while (condition) {
+    
+}
+
+do {
+    
+} while (condition);
