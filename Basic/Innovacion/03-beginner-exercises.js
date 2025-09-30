50c07aff85edb74b036b3f847c5e14d2369f8022
innovacion3 <innovaciontecnologica3@hvvaldecilla.es>
2025-09-30 08:25:29 +0200

    Primer bloque de ejercicios completado

---
+++ Basic/Innovacion/03-beginner-exercises.js
@@ -0,0 +1,91 @@
+/*
+Clase 18 - Ejercicios: primeros pasos
+Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
+*/
+
+// 1. Escribe un comentario en una línea
+// Esto es un comentario
+
+// 2. Escribe un comentario en varias líneas
+/* 
+esto 
+es 
+un comentaio en 
+varias lineas */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let string = "string"
+let number = 23
+let double = 2.3
+let boolean = true
+let undefined
+let nullValue = null
+let mySymbol = Symbol
+let bigInt = 526173894012
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(string)
+console.log(number)
+console.log(double)
+console.log(undefined)
+console.log(nullValue)
+console.log(mySymbol)
+console.log(boolean)
+console.log(bigInt)
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof string)
+console.log(typeof number)
+console.log(typeof double)
+console.log(typeof undefined)
+console.log(typeof nullValue)
+console.log(typeof mySymbol)
+console.log(typeof boolean)
+console.log(typeof bigInt)
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+string = "cadena de texto"
+number = 24
+double = 2.4
+boolean = false
+undefined
+nullValue = null
+mySymbol = Symbol
+bigInt = 526173894013
+
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+string = true
+number = "number"
+double = 2
+boolean = null
+undefined
+nullValue = 40.86
+mySymbol = Symbol
+bigInt = "526173894013"
+
+console.log("***********************")
+console.log(typeof string)
+console.log(typeof number)
+console.log(typeof double)
+console.log(typeof undefined)
+console.log(typeof nullValue)
+console.log(typeof mySymbol)
+console.log(typeof boolean)
+console.log(typeof bigInt)
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const string2 = "string"
+const number2 = 23
+const double2 = 2.3
+const boolean2 = true
+// const undefined
+const nullValue2 = null
+const mySymbol2 = Symbol
+const bigInt2 = 526173894012
+
+// 9. A continuación, modifica los valores de las constantes
+// Los  valores CONST no pueden ser cambiados
+
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
