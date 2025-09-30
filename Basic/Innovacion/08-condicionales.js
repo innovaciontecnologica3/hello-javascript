9b06f0a35032c648598384f79b6e7eed9abef203
innovacion3 <innovaciontecnologica3@hvvaldecilla.es>
2025-09-30 11:40:48 +0200

    Refrescando condicionales

---
+++ Basic/Innovacion/08-condicionales.js
@@ -0,0 +1,20 @@
+// if, else if, else
+let edad = 23
+
+if (edad == 50) {
+    console.log("eres viejo")
+} else if (edad < 18) {
+    console.log("vete al cole!!")
+} else {
+    console.log("pareces mas joven")
+}
+
+// switch
+switch (edad) {
+    case 18:
+
+        break;
+
+    default:
+        break;
+}
